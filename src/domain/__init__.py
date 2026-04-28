35ed90160625acb3d3aaa7c8005ed125a17703e3
Сергей Савушкин <129319104+SavushkinSV@users.noreply.github.com>
2026-04-28 21:10:49 +0300

    Update __init__.py

--- src/domain/__init__.py
+++ src/domain/__init__.py
@@ -1,6 +1,1 @@
-"""Импорты слоя service."""
 
-from domain.service.game_service import GameService
-from domain.service.game_service_impl import GameServiceImpl
-
-__all__ = ["GameService", "GameServiceImpl"]
